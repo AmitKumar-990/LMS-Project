54800ebfd24da754df6c0d431bab2575cb3a4feb
AmitKumar-990 <amitk994462@gmail.com>
2025-11-25 10:57:33 +0530

    changes in user

---
+++ user/src/component/educator/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div><h1>Educator Navbar</h1></div>
+  )
+}
+
+export default navbar
---
+++ user/src/component/student/CallToAction.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <div>call to action</div>
+  )
+}
+
+export default CallToAction
